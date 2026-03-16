61506870cfc8736c2db767732b754a31cb4d887c
vzdzrj75v6-ui <vzdzrj75v6@privaterelay.appleid.com>
2026-03-16 17:44:19 +0700

    Update matrix.js

--- matrix.js
+++ matrix.js
@@ -1,13 +1,12 @@
 const canvas=document.getElementById("matrix");
 const ctx=canvas.getContext("2d");
 
-canvas.height=window.innerHeight;
 canvas.width=window.innerWidth;
+canvas.height=window.innerHeight;
 
-const letters="01ENGINECODE";
-const fontSize=14;
-
-const columns=canvas.width/fontSize;
+const letters="01ENGINE";
+const size=16;
+const columns=canvas.width/size;
 
 const drops=[];
 
@@ -19,19 +18,19 @@ drops[x]=1;
 
 function draw(){
 
-ctx.fillStyle="rgba(0,0,20,0.1)";
+ctx.fillStyle="rgba(0,0,0,0.05)";
 ctx.fillRect(0,0,canvas.width,canvas.height);
 
-ctx.fillStyle="#00aaff";
-ctx.font=fontSize+"px monospace";
+ctx.fillStyle="#00c3ff";
+ctx.font=size+"px monospace";
 
 for(let i=0;i<drops.length;i++){
 
-let text=letters[Math.floor(Math.random()*letters.length)];
+const text=letters[Math.floor(Math.random()*letters.length)];
 
-ctx.fillText(text,i*fontSize,drops[i]*fontSize);
+ctx.fillText(text,i*size,drops[i]*size);
 
-if(drops[i]*fontSize>canvas.height&&Math.random()>0.975){
+if(drops[i]*size>canvas.height && Math.random()>0.975){
 
 drops[i]=0;
 
